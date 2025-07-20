5d88b5b7eece0c295b47bdb6581d57eb1a2b106a
Andy Toala <aj.toalaa@uea.edu.ec>
2025-07-19 19:52:30 -0500

    Semana 8 - Practico Experimental

--- obj/Debug/net9.0/Proyecto1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Proyecto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ca076cad8a89370cc40c63bd01c75d1fac8629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfeca1efc09a4dae24374a1322f616f8c3048a96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
