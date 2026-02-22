f432264ec6fc51d444ecc9d72bb2aade75cfe14a
Andy Jair Toala <aj.toalaa@uea.edu.ec>
2026-02-22 17:55:36 -0500

    Semana 10 - Sistema de Turnos Flask

--- obj/Debug/net9.0/Proyecto1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Proyecto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10cca414ad1bad39d95770db0b37f0e423eb937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d98762c9fd56c30c4bfea5ea42dcf33661fecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
