63fb8e243249e900c954ee3fb0f89bb214c1ee2b
Andy Jair Toala <aj.toalaa@uea.edu.ec>
2026-02-22 17:19:34 -0500

    Semana 10 - Agregar plantillas con herencia Jinja2 y páginas dinámicas

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-    return "Bienvenido al Sistema de Turnos – Clínica XYZ"
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/usuario/<nombre>')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/usuario/<nombre>")
 def usuario(nombre):
-    return f"Bienvenido, {nombre}. Tu turno está en proceso."
+    return render_template("usuario.html", nombre=nombre)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+    
