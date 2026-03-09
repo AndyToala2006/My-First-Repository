99ee3ff48d109b91b464fa77b3e7ff1a88a28f46
Andy Jair Toala <aj.toalaa@uea.edu.ec>
2026-03-08 20:43:56 -0500

    Semana 12 - Desarrollo de Aplicaciones Web

---
+++ Sistema_Turnos_Flask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/usuario/<nombre>")
+def usuario(nombre):
+    return render_template("usuario.html", nombre=nombre)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
